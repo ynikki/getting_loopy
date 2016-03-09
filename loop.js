a5d1ee8259acb902b9d4e2cfc1faaa6d214cffee
Nikki Y Kobayashi <nikz@MacBook-Pro.local>
2016-03-08 21:18:01 -1000

    Finished printContent

---
+++ loop.js
@@ -0,0 +1,16 @@
+/* GETTING L00PY */
+
+/* PRESIDENTS - ACCESSING VALUES IN AN ARRAY */
+
+var presidents = ["Washington", "Adams", "Jefferson", "Madison", "Monroe"];
+
+function printContent(arr){
+	for(var i = 0; i<arr.length; i++){
+		console.log("Value of i is : " + i);
+		console.log("Value at Index is: " + arr[i]);
+	} return "hey";
+}
+
+console.log(printContent(presidents));
+
+
